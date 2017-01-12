657c87d423d36d38c15f602bae6770b9e1c166e4
manan <aoy.mananchaya@gmail.com>
2017-01-12 20:57:16 +0700

    login Activity

--- app/src/main/java/cskku/amornpalang/mananchaya/petpet/loginActivity.java
+++ app/src/main/java/cskku/amornpalang/mananchaya/petpet/loginActivity.java
@@ -1,13 +1,41 @@
 package cskku.amornpalang.mananchaya.petpet;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class loginActivity extends AppCompatActivity {
+    //Explicit
+    private Button btnLoginButton,btnGuestButton,btnRegisterButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-    }
-}
+
+        btnLoginButton = (Button) findViewById(R.id.btnLogin);
+        btnGuestButton = (Button) findViewById(R.id.btnGuest);
+        btnRegisterButton = (Button) findViewById(R.id.btnRegister);
+
+        // login controller
+        btnLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(loginActivity.this, selectOneActivity.class));
+            }
+        });
+
+        // guest controller
+        btnGuestButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(loginActivity.this,selectOneActivity.class));
+
+            }
+        });
+
+
+    }//main mathon
+}//main class
