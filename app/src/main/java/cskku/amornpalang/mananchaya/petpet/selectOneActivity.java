9af3cbfe1d81176608b8cef087970442498bc56a
manan <aoy.mananchaya@gmail.com>
2017-01-12 16:53:19 +0700

    select page

---
+++ app/src/main/java/cskku/amornpalang/mananchaya/petpet/selectOneActivity.java
@@ -0,0 +1,13 @@
+package cskku.amornpalang.mananchaya.petpet;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class selectOneActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_one);
+    }
+}
