dcd50cb845ab214a8fd475edbbcca0b852098aaa
manan <aoy.mananchaya@gmail.com>
2017-01-12 20:41:23 +0700

    main Activity

--- app/src/main/java/cskku/amornpalang/mananchaya/petpet/MainActivity.java
+++ app/src/main/java/cskku/amornpalang/mananchaya/petpet/MainActivity.java
@@ -1,13 +1,37 @@
 package cskku.amornpalang.mananchaya.petpet;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 public class MainActivity extends AppCompatActivity {
+    //Explicit
+    private Button nextButton;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+        nextButton = (Button) findViewById(R.id.next);
+
+        //next controller
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, loginActivity.class));
+            }
+
+        });
+    }//main method
+}//main class
