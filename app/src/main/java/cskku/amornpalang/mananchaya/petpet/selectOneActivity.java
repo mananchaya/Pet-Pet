46a86b37f31aa448321f5b70912966330b7fc93a
manan <aoy.mananchaya@gmail.com>
2017-01-13 00:14:42 +0700

    edit layout

--- app/src/main/java/cskku/amornpalang/mananchaya/petpet/selectOneActivity.java
+++ app/src/main/java/cskku/amornpalang/mananchaya/petpet/selectOneActivity.java
@@ -1,13 +1,33 @@
 package cskku.amornpalang.mananchaya.petpet;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class selectOneActivity extends AppCompatActivity {
+
+    //Explicit
+    private Button btnMydogButton,btnStrayDogButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_one);
-    }
-}
+        btnMydogButton = (Button) findViewById(R.id.btnMyDog);
+        btnStrayDogButton = (Button) findViewById(R.id.btnStrayDog);
+        // btn My Dog Controller
+        btnMydogButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(selectOneActivity.this,myDogActivity.class));
+            }
+        });
+
+        //btn Stray Dog Controller
+
+
+    }//main method
+}//main class
